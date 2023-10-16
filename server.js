bfdc9c89f3efa87229362fac142171d06d5f889f
Sean Tierney <stierney0505@gmail.com>
2023-10-16 17:13:54 -0400

    Quick small changes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const authController = require('./controllers/authenicate')
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 
 //routes
