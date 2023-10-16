41413d856bcacf2228c49b425ab381270e691cf4
Adam Hughes <hugham08@pfw.edu>
2023-10-16 16:27:20 -0400

    this is the start of the new thing other bad

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+a
 const routes: Routes = [];
 
 @NgModule({
