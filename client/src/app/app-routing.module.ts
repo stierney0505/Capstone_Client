c393832fc1c2d1859a589524dbd07a7e76485394
Adam Hughes <hugham08@pfw.edu>
2023-11-03 11:16:19 -0400

    added header to dashboard component and added dashboard route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { ConfirmEmailComponent } from './confirmemail/email.component';
+import { FacultyDashboardComponent } from './faculty-dashboard/faculty-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
   { path: 'signup', component: SignupComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'faculty-dashboard', component: FacultyDashboardComponent},
   { path: "confirm-email/:emailtoken", component: ConfirmEmailComponent}
 ];
 
