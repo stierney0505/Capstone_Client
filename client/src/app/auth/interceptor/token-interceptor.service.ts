a862cf99538c79d4b22d1c8ce8779ab5222b1508
Adam Hughes <hugham08@pfw.edu>
2023-11-03 11:37:38 -0400

    generated route guard and interceptor code

---
+++ client/src/app/auth/interceptor/token-interceptor.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('jwtToken');
+
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+    }
+    return next.handle(req);
+  }
+}
